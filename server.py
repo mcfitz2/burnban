4cba1546beef47e13bf7c50fde80ec9a373337b8
Micah Fitzgerald <mcfitz2@gmail.com>
2018-09-25 00:16:19 +0000

    rebuild with Postgres DB, added flake8 config, dev requirements

--- server.py
+++ server.py
@@ -1,93 +1,105 @@
-from flask import Flask, request, abort
-import requests, json
-import datetime, os
+from flask import Flask, abort
+import requests
+import json
+import sys
+import datetime
+import os
 import xml.etree.ElementTree
-from dateutil.parser import parse
-from json import JSONDecoder
-from json import JSONEncoder
+from peewee import fn, Model, DateTimeField, BooleanField, CharField, DoesNotExist
+from playhouse.db_url import connect
+from playhouse.shortcuts import model_to_dict
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 
 app = Flask(__name__)
-data = None
+db = connect(os.environ['DATABASE_URL'])
 
 
+def get_banned_counties():
+    r = requests.get(os.environ['BURNBAN_URL'])
+    e = xml.etree.ElementTree.fromstring(r.content)
+    counties = ([i.strip() for i in e.find('channel').find('item').find('description').text.split(',')])
+    logging.debug(counties)
+    return counties
 
-class DateTimeDecoder(json.JSONDecoder):
 
-    def __init__(self, *args, **kargs):
-        JSONDecoder.__init__(self, object_hook=self.dict_to_object,*args, **kargs)
-    def dict_to_object(self, d): 
-        if '__type__' not in d:
-            return d
-        type = d.pop('__type__')
-        try:
-            dateobj = datetime.datetime(**d)
-            return dateobj
-        except Exception as e:
-            print(e)
-            d['__type__'] = type
-            return d
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-class DateTimeEncoder(JSONEncoder):
-    """ Instead of letting the default encoder convert datetime to string,
-        convert datetime objects into a dict, which can be decoded by the
-        DateTimeDecoder
-    """
+    def to_json(self):
+        d = {k: v.isoformat() if isinstance(v, datetime.datetime) else v for k, v in model_to_dict(self).items() if k != "id"}
+        return json.dumps(d)
 
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
-            return {
-                '__type__' : 'datetime',
-                'year' : obj.year,
-                'month' : obj.month,
-                'day' : obj.day,
-                'hour' : obj.hour,
-                'minute' : obj.minute,
-                'second' : obj.second,
-                'microsecond' : obj.microsecond,
-            }
-        else:
-            return JSONEncoder.default(self, obj)
 
-with open('data.json') as f:
-	data = json.load(f, cls=DateTimeDecoder)
-print(data['updated_date'])
-def update_bans():
-	if not 'updated_date' in data.keys() or not data['updated_date'] or (data['updated_date'] < datetime.datetime.now()-datetime.timedelta(hours=24)):
-		r = requests.get('http://tfsfrp.tamu.edu/WILDFIRES/BURNBAN.XML')
-		e = xml.etree.ElementTree.fromstring(r.content)
-		counties = ([i.strip() for i in e.find('channel').find('item').find('description').text.split(',')])
-		data['updated_date'] = datetime.datetime.now()
-		for county_name, county in data['counties'].items():
-			if county_name in counties:
-				data['counties'][county_name.upper()]['burn_ban'] = True
-			else:
-				data['counties'][county_name.upper()]['burn_ban'] = False
-		with open('data.json', 'w') as f:
-			json.dump(data, f, cls=DateTimeEncoder)
+class County(BaseModel):
+    name = CharField(unique=True)
+    burn_ban = BooleanField()
+    updated_date = DateTimeField(null=True)
 
-update_bans()
+    @classmethod
+    def _get_earliest_update(cls):
+        return cls.select(fn.Min(cls.updated_date)).scalar()
 
-def get_county(lat, lng):
-	r = requests.get("https://geo.fcc.gov/api/census/area", params={"lat":lat, "lon":lng, "format":"json"})
-	return r.json()['results'][0]['county_name'].upper()
+    @classmethod
+    def update_bans(cls):
+        if cls._get_earliest_update() < (datetime.datetime.utcnow() - datetime.timedelta(seconds=1)):
+            counties = get_banned_counties()
+            with cls._meta.database.atomic():
+                cls.update(burn_ban=False, updated_date=datetime.datetime.utcnow()).execute()
+                cls.update(burn_ban=True, updated_date=datetime.datetime.utcnow()).where(County.name << counties).execute()
 
-@app.route('/county/<county>')
-def by_county(county):
-	update_bans()
-	if county in data["counties"]:
-		return json.dumps({"burn_ban":data["counties"][county.upper()]["burn_ban"], "updated_date":data["updated_date"].isoformat()}, cls=DateTimeEncoder)
-	else:
-		return abort(404)
+
+def get_county_from_location(lat, lng):
+    r = requests.get(os.environ['FCC_URL'], params={"lat": lat, "lon": lng, "format": "json"})
+    return r.json()['results'][0]['county_name'].upper()
+
+
+def get_county_from_address(query):
+    r = requests.get(os.environ['NOMINATUM_URL'], params={"q": query, "format": "json"}).json()[0]
+    return get_county_from_location(r['lat'], r['lon'])
+
+
+@app.route('/county/<county_name>')
+def by_county(county_name):
+    County.update_bans()
+    try:
+        county = County.get(name=county_name.upper())
+        return county.to_json()
+    except DoesNotExist:
+        return abort(404)
+
+
 @app.route('/location/<lat>/<lng>')
 def by_location(lat, lng):
-	update_bans()
-	county = get_county(lat, lng)
-	return json.dumps({"burn_ban":data["counties"][county.upper()]["burn_ban"], "updated_date":data["updated_date"].isoformat()}, cls=DateTimeEncoder)
+    County.update_bans()
+    county_name = get_county_from_location(lat, lng)
+    try:
+        county = County.get(name=county_name.upper())
+        return county.to_json()
+    except DoesNotExist:
+        return abort(404)
+
 
 @app.route('/place/<place>')
 def by_place(place):
-	update_bans()
+    County.update_bans()
+    county_name = get_county_from_address(place)
+    try:
+        county = County.get(name=county_name.upper())
+        return county.to_json()
+    except DoesNotExist:
+        return abort(404)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.environ['PORT'], host="0.0.0.0")
+    command = sys.argv[1]
+    if command == "run":
+            app.run(debug=True, port=os.environ['PORT'], host="0.0.0.0")
+    elif command == "init":
+        db.execute_sql("DROP TABLE county CASCADE;")
+        db.create_tables([County])
+        sql_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'initialize.sql')
+        with open(sql_path, 'r') as sql_file:
+            db.execute_sql(sql_file.read())
